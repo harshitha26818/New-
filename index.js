7c76be94daa1f48bae5aed3d1cbf5b54cd5b7f6b
harshitha26818 <harshithakotla2689@gmail.com>
2021-05-01 16:20:49 +0530

    hello peps

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express=require('express');
 var app=express();
 var port=process.env.PORT ||3000;
-;
+
 app.get('/',function(req,res){
-    res.send("hello thaliva");
+   // res.send("hello thaliva");
+   res.sendFile(__dirname+'/frontend/html/index.html');
 });
 
 app.listen(port,function(){
